a4af36c1804c5cae048e4d3c850a49a0f089060f
trungld3 <k21tpm4@gmail.com>
2020-04-29 00:21:09 +0700

    Initial Commit

---
+++ CoreMLSeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLSeeFood
+//
+//  Created by TrungLD on 4/29/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
